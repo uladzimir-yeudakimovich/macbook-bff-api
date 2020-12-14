edd7ff7b9a30c4da5d753618d4e717830750a9f6
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2020-12-14 19:57:05 +0100

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+
+import * as helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 4000;
@@ -9,6 +12,7 @@ async function bootstrap() {
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
+  app.use(helmet());
   
   await app.listen(port);
 }
